504546433403d573124c64e73a6699508e41df5a
Tarun Warrier <tarunwarrier1@gmail.com>
2025-06-16 15:03:16 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/asl_converter_website",
   resolve: {
     alias: {
       "@":path.resolve(__dirname, "./src"),
